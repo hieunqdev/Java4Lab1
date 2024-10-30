c42b4757c31e9221ec53584d0d48e9568fbaae39
hieunq23 <hieunq23@fpt.edu.vn>
2024-10-30 14:03:00 +0700

    ket noi csdl

---
+++ src/main/java/model/DBTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		// kết nối csdl
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("PolyOE");
+		
+		// truy vấn csdl
+		EntityManager em = factory.createEntityManager();
+		
+		System.out.println("kết nối thành công");
+	}
+
+}
